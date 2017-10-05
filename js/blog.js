ac7480afccb7c50b18dbc838824f682f8dcb257a
PhoenixPan <phoenixpan1992@gmail.com>
2017-10-05 22:38:24 +1100

    restore

---
+++ js/blog.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Phoenix on 2/19/2017.
+ */
+// Scroll the page down smoothly
+$(function() {
+    $('a').on('click', function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 800);
+                return false;
+            }
+        }
+    });
+});
--- source/_posts/Test-post.md
+++
@@ -1,7 +0,0 @@
----
-title: Test post
-date: 2017-10-05 21:20:57
-tags:
----
-
-## Hahahaha
